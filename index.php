06b2ec08de121f802bf71e4ccedf59c18992b1f4
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-01-09 15:19:38 -0300

    config routes in index

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
 
-$url_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ; 
+$url_path  = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') ; 
+$url_parts = explode('/', $url_path);
 
-//implementar aqui a interpretação da url e instacia controllers .... 
+$controller = $url_parts[1] ?? 'home'   ;
+$method     = $url_parts[2] ?? 'index'  ; 
+$params     = array_slice($url_parts, 3);
+
+// find controller file.
+$controller     = ucfirst($controller);
+$controllerFile =  __DIR__ . "/controllers/{$controller}.php";
+
+//verify if controller exists.
+if(!file_exists($controllerFile)) {
+    $controllerFile =  __DIR__ . "/controllers/Home.php";
+}
+    
+require_once $controllerFile;
+
+//verify if class and method exists in controller.
+if (!class_exists($controller) || !method_exists($controller, $method)) {
+    require_once __DIR__ . "/controllers/Home.php";
+    $controller = 'Home';
+    $method     = 'index';
+    $params     = [];
+} 
+
+$controllerInstance = new $controller();
+call_user_func_array([$controllerInstance, $method], $params);
 
 ?>
