d2cb4d427a141ab7870aacff0a72868f089e52c7
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-01-10 21:47:15 -0300

    login html and css

---
+++ views/login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Workout Wise</title>
+    <link rel="stylesheet" href="/workout-wise/assets/css/reset.css">
+    <link rel="stylesheet" href="/workout-wise/assets/css/login.css">
+    <script defer src=""></script>
+</head>
+<body>
+    
+    <main>
+
+        <section id="apresentation" class="apresentation">
+
+            <div class="info" id="info">
+
+
+                <div class="imgs-wrapper">
+                    <div class="img-wrapper">
+                        <img src="/workout-wise/assets/img/academia.png" alt="">
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="/workout-wise/assets/img/biceps.png" alt="">
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="/workout-wise/assets/img/levantamento-de-peso.png" alt="">
+                    </div>
+                </div>
+               
+
+                <p>
+                    Workout Wise é um software que te ajuda a acompanhar e gerenciar sua evolução na academia.
+                </p>
+                <p>
+                    Você pode:
+                </p>
+                <ul>
+                    <li>Registrar seus treinos, exercícios, peso, séries e repetições</li>
+                    <li>Manter um histórico detalhado de seus treinos</li>
+                    <li>Obter uma análise automatizada sobre seu progresso</li>
+                </ul>
+            </div>
+            
+
+        </section>
+        <section id="form-div" class="form-div">
+
+            <form id="form-login">
+
+                <div class="do-your-login">
+                    <p>Faça seu Login</p>
+                    <span class="spacer-span"></span>
+                </div>
+                
+                <div class="form-wrapper">
+                    <label for="email">E-mail</label>
+                    <input type="text" name="email" id="email">
+                </div>
+
+                <div class="form-wrapper">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <button type="submit">Entrar</button>
+
+                <a href="authentication/open_register_page">Ainda não possui uma conta? Crie uma agora!</a>
+
+            </form>
+    
+        </section>
+
+    </main>
+
+</body>
+</html>
