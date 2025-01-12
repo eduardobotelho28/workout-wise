1ee22e5ff9c05b4e8269fefff87fbb702d5324a2
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-01-11 22:33:46 -0300

    register html adn css

--- views/register.php
+++ views/register.php
@@ -1,11 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Workout Wise</title>
+    <link rel="stylesheet" href="/workout-wise/assets/css/reset.css">
+    <link rel="stylesheet" href="/workout-wise/assets/css/register.css">
+    <script defer src=""></script>
 </head>
 <body>
-    registro
+    
+    <main>
+
+        <section id="form-div" class="form-div">
+
+            <form id="form-register">
+
+                <div class="do-your-register">
+                    <p>Faça seu Cadastro!</p>
+                    <span class="spacer-span"></span>
+                </div>
+                
+                <div class="form-wrapper">
+                    <label for="name">Nome Completo</label>
+                    <input type="text" name="name" id="name">
+                </div>
+
+                <div class="form-wrapper">
+                    <label for="email">E-mail</label>
+                    <input type="text" name="email" id="email">
+                </div>
+
+                <div class="form-wrapper">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <div class="form-wrapper">
+                    <label for="password">Confirmar Senha</label>
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <button type="submit">Cadastrar</button>
+
+                <div id="msg-response" class="msg-response">
+               
+                </div>
+
+            </form>
+
+            
+    
+        </section>
+
+        <section id="apresentation" class="apresentation">
+
+            <div class="info" id="info">
+
+
+                <div class="imgs-wrapper">
+                    <div class="img-wrapper">
+                        <img src="/workout-wise/assets/img/academia.png" alt="">
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="/workout-wise/assets/img/biceps.png" alt="">
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="/workout-wise/assets/img/levantamento-de-peso.png" alt="">
+                    </div>
+                </div>
+               
+                <p>
+                    Workout Wise é um software que te ajuda a acompanhar e gerenciar sua evolução na academia.
+                </p>
+                <p>
+                    Você pode:
+                </p>
+                <ul>
+                    <li>Registrar seus treinos, exercícios, peso, séries e repetições</li>
+                    <li>Manter um histórico detalhado de seus treinos</li>
+                    <li>Obter uma análise automatizada sobre seu progresso</li>
+                </ul>
+            </div>
+            
+
+        </section>
+
+    </main>
+
 </body>
 </html>
